99195724b80d9cb4fb6ff19c22dba8397d67225e
dhivyasekar0112 <109870707+dhivyasekar0112@users.noreply.github.com>
2024-02-24 15:12:08 +0530

    Update srm_query_app.py

--- srm_query_app.py
+++ srm_query_app.py
@@ -1,4 +1,4 @@
-pip install -i https://pypi.org/simple/ bitsandbytes
+pip install accelerate
 import streamlit as st
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
